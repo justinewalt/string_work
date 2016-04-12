a9889d611967d8861ce451855a2a3d957e0b213b
Justin Ewalt <justinewalt@gmail.com>
2016-04-12 10:15:25 -0600

    built out specs

--- spec/string_work_justinewalt_spec.rb
+++ spec/string_work_justinewalt_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe StringWorkJustinewalt do
+  subject {StringWorkJustinewalt}
+
   it 'has a version number' do
     expect(StringWorkJustinewalt::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  describe 'manipulate strings' do
+  	let(:str) {'My String'}
+
+  	it 'reverses a string' do
+  		expect(subject.reversify(str)).to eq('gnirtS yM')
+  	end
+
+	  it 'adds spaces' do
+	  	expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+	  end
+	end
 end
